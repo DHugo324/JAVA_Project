9f4c27d10d3a9b5d83fec2c6934f70fa278f09e4
Gaku327 <liyuesa0616@gmail.com>
2023-05-24 22:00:28 +0800

    Update LoginBoardMain.java

--- LoginBoardMain.java
+++ LoginBoardMain.java
@@ -18,9 +18,9 @@ public class LoginBoardMain extends JFrame{
     }
     public LoginBoardMain() {
         super("Login");
-        setBackground(Color.WHITE);
+        setBackground(Color.PINK);
         setLayout(new GridBagLayout());
-    
+        JPanel titlePanel = new JPanel(new GridLayout(1, 1));
         JPanel accountPanel = new JPanel(new GridLayout(2, 1));
         JPanel passwordPanel = new JPanel(new GridLayout(2, 1));
     
@@ -29,19 +29,25 @@ public class LoginBoardMain extends JFrame{
         JTextField passwordText = new JTextField();
         passwordText.setPreferredSize(new Dimension(250, 25));
         JButton loginButton = new JButton("LOGIN");
-    
+        JLabel titleLabel = new JLabel("Y o u r C l a s s");
+        titleLabel.setFont(new Font("Arial", Font.PLAIN, 40));
+        titlePanel.add(titleLabel);
         JLabel accountLabel = new JLabel("Account:");
+        accountLabel.setFont(new Font("Arial", Font.PLAIN, 20));
         accountPanel.add(accountLabel);
         accountPanel.add(accountText);
     
         JLabel passwordLabel = new JLabel("PassWord:");
+        passwordLabel.setFont(new Font("Arial", Font.PLAIN, 20));
         passwordPanel.add(passwordLabel);
         passwordPanel.add(passwordText);
     
         GridBagConstraints gbc = new GridBagConstraints();
 
-        gbc.insets = new Insets(10, 10, 10, 10); // 設定元件間距
+        gbc.insets = new Insets(10, 10, 5, 10); // 設定元件間距
         gbc.gridx = 0;
+        gbc.gridy = 0;
+        add(titlePanel,gbc);
         gbc.gridy = 1;
         add(accountPanel, gbc);
     
