d6a5e89527c65f148feb598500abd3d4bb7ef569
DHugo324 <hugodai0324@gmail.com>
2023-06-06 17:13:20 +0800

    update

--- loginSystem/IPaddress.java
+++
@@ -1,8 +0,0 @@
-package loginSystem;
-
-public interface IPaddress {
-  final String ip = "IP地址";
-  final String sqldbaccount = "root";
-  final String sqldbpass = "使用者密碼";
-  final String dbName = "資料庫名稱";
-}
