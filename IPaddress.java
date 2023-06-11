7e8c8c660c431bb09bfc6507ea84a7b93fba08bf
DHugo324 <hugodai0324@gmail.com>
2023-06-11 21:12:11 +0800

    Update IPaddress.java

--- IPaddress.java
+++ IPaddress.java
@@ -3,6 +3,6 @@ package java2023.project;
 public interface IPaddress {
   final String ip = "localhost:3306";
   final String sqldbaccount = "root";
-  final String sqldbpass = "";
-  final String dbName = "account";
+  final String sqldbpass = "使用者密碼";
+  final String dbName = "資料庫名字";
 }
